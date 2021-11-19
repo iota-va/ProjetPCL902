e6e0620c2db8319fb5e59e13ef9ddfe68d0db491
iota <vincent.alfonso@hotmail.fr>
2021-11-19 12:30:56 +0100

    Nouvelle interface

--- Interface.py
+++ Interface.py
@@ -1,141 +1,144 @@
-"""
-Created on Fri Sep 24 09:00 2021
-@author:DEPELSEMACKER Karl, ALFONSO Vincent, KESTEL Samuel
-"""
-
 from tkinter import *
+from tkinter import ttk
+import numpy as np
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
+NavigationToolbar2Tk)
 import matplotlib.pyplot as plt
-import pyfirmata
-import time as t
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
-from matplotlib.backend_bases import key_press_handler
-from matplotlib.figure import Figure
-import pyvisa
-from DetectionSynchrone import *
-import serial
 
-plt.style.use('seaborn')
 
-class GUI():
-    def __init__(self,root):
-        self.root = root
-        self.root.title("Aquisiteur 9000")
+class Interface:
 
-        self.buttons = LabelFrame(self.root, text="Buttons", bg= 'white')
-        self.buttons.grid(row=0,column=0)
+    def __init__(self, master):
+        self.master = master
+        master.title('PCL902')
 
-        self.para = LabelFrame(self.root, text="Paramètres")
-        self.para.grid(row=1,column=0)
+        self.master.geometry("800x600")
+        self.master.resizable(width=False, height=False)
 
-        self.button1 = Button(self.buttons, text="Moteur", command=self.moteur, font=('Courier',15), bg='white')
-        self.button1.grid(row=0,rowspan=1,column=0)
+        self.notebook = ttk.Notebook(master)
+        self.notebook.grid()
 
-        self.button2 = Button(self.buttons, text="Graph", command=self.graph, font=('Courier',15), bg='white')
-        self.button2.grid(row=0,rowspan=1,column=2)
+        self.tabMotor, self.tabLockin, self.tabGraphic, self.tabFit, self.tabMeasure = ttk.Frame(self.notebook,width=790,height=590), ttk.Frame(self.notebook, width=790, height=590), ttk.Frame(self.notebook, width=790, height=590), ttk.Frame(self.notebook, width=790, height=590), ttk.Frame(self.notebook, width=790, height=590)
 
-        self.button3 = Button(self.buttons, text="Curve fit", command=self.courbe, font=('Courier',15), bg='white')
-        self.button3.grid(row=0,rowspan=1,column=3)
-
-        self.button4 = Button(self.buttons, text = "Lock-IN", command=self.lock, font=('Courier',15), bg='white')
-        self.button4.grid(row=0,rowspan=1,column=1)
-
-        self.button5 = Button(self.buttons, text="Mesure", command=self.mesure, font=('Courier',15), bg='white')
-        self.button5.grid(row=0,rowspan=1,column=4)
-
-    def moteur(self):
-
-        self.Lmot = LabelFrame(self.para,text = "Moteur")
-        self.Lmot.grid(row=0,column=0)
+        self.notebook.add(self.tabMotor, text='Motor')
+        self.notebook.add(self.tabLockin, text='Lock-in')
+        self.notebook.add(self.tabGraphic, text='Graphic')
+        self.notebook.add(self.tabFit, text='Curve fit')
+        self.notebook.add(self.tabMeasure, text='Measure')
+        
+        # Motor :
 
         self.v1, self.v2, self.v3, self.v4 = DoubleVar(), DoubleVar(), DoubleVar(), DoubleVar()
-        
-        Label(self.Lmot,text='angle', font=('Courier',15)).grid(row=0, column=0)
-        self.angle = Entry(self.Lmot, textvariable=self.v1, width=5, font=('Courier',15))
-        self.angle.grid(row=0, column=1)
 
-        Label(self.Lmot,text='pas', font=('Courier',15)).grid(row=1, column=0)
-        self.pas = Entry(self.Lmot, textvariable=self.v2, width=5, font=('Courier',15))
-        self.pas.grid(row=1, column=1)
+        Label(self.tabMotor, text='Angle', font=(24)).place(x=150, y=100)
+        self.angle = Entry(self.tabMotor, textvariable=self.v1, justify='center', width=10, font=(24))
+        self.angle.place(x=220, y=100, height=30)
 
-        Label(self.Lmot,text="temps d'integration", font=('Courier',15)).grid(row=2, column=0)
-        self.tps = Entry(self.Lmot, textvariable=self.v3, width=5, font=('Courier',15))
-        self.tps.grid(row=2, column=1)
+        Label(self.tabMotor, text='Pas', font=(24)).place(x=160, y=250)
+        self.pas = Entry(self.tabMotor, textvariable=self.v2, justify='center', width=10, font=(24))
+        self.pas.place(x=220, y=250, height=30)
 
-        Label(self.Lmot,text="step", font=('Courier',15)).grid(row=3, column=0)
-        self.step = Entry(self.Lmot, textvariable=self.v4, width=5, font=('Courier',15))
-        self.step.grid(row=3, column=1)
+        Label(self.tabMotor, text="Integration Time", font=(24)).place(x=420, y=100)
+        self.tps = Entry(self.tabMotor, textvariable=self.v3, justify='center', width=10, font=(24))
+        self.tps.place(x=570, y=100, height=30)
 
-        self.app1 = Button(self.Lmot, text="Appliquer", command=self.appmot, font=('Courier',15))
-        self.app1.grid(row=4, column=0,padx=10)
+        Label(self.tabMotor, text="Step", font=(24)).place(x=500, y=250)
+        self.step = Entry(self.tabMotor, textvariable=self.v4, justify='center', width=10, font=(24))
+        self.step.place(x=570, y=250, height=30)
 
-        self.kill = Button(self.Lmot, text="Quitter", command=lambda:self.Lmot.destroy(), font=('Courier',15))
-        self.kill.grid(row=4, column=1)
+        self.app1 = Button(self.tabMotor, text="Apply", command=self.appmot, width=20, height=5, font=(24))
+        self.app1.place(x=320, y=400)
 
-    def lock(self):
-        self.w1, self.w2, self.w3, self.w4 = DoubleVar(), DoubleVar(), DoubleVar(), DoubleVar()
+        # Lock-in
 
-        self.Llock = LabelFrame(self.para,text = "Lock-In")
-        self.Llock.grid(row=0,column=1)
+        self.w1, self.w2, self.w3, self.w4, self.w5, self.w6, self.w7, self.w8, self.w9, self.w10 = DoubleVar(), DoubleVar(), DoubleVar(), DoubleVar(), DoubleVar(), DoubleVar(), DoubleVar(), DoubleVar(), DoubleVar(), DoubleVar()
 
-        Label(self.Llock,text='Amplitude', font=('Courier',15)).grid(row=0, column=0)
-        self.amp = Entry(self.Llock, textvariable=self.w1, width=5, font=('Courier',15))
-        self.amp.grid(row=0, column=1)
+        Label(self.tabLockin, text='Amplitude', font=(24)).place(x=120, y=67)
+        self.amp = Entry(self.tabLockin, textvariable=self.w1, justify='center', width=10, font=(24))
+        self.amp.place(x=220, y=67)
 
-        Label(self.Llock,text='fq', font=('Courier',15)).grid(row=1, column=0)
-        self.fq = Entry(self.Llock, textvariable=self.w2, width=5, font=('Courier',15))
-        self.fq.grid(row=1, column=1)
+        Label(self.tabLockin, text='Frequency', font=(24)).place(x=115, y=134)
+        self.fq = Entry(self.tabLockin, textvariable=self.w2, justify='center', width=10, font=(24))
+        self.fq.place(x=220, y=134)
 
-        Label(self.Llock,text="off", font=('Courier',15)).grid(row=2, column=0)
-        self.off = Entry(self.Llock, textvariable=self.w3, width=5, font=('Courier',15))
-        self.off.grid(row=2, column=1)
+        Label(self.tabLockin, text='Harmonic', font=(24)).place(x=120, y=201)
+        self.harm = Entry(self.tabLockin, textvariable=self.w3, justify='center', width=10, font=(24))
+        self.harm.place(x=220, y=201)
 
-        Label(self.Llock,text="harm", font=('Courier',15)).grid(row=3, column=0)
-        self.harm = Entry(self.Llock, textvariable=self.w4, width=5, font=('Courier',15))
-        self.harm.grid(row=3, column=1)
+        Label(self.tabLockin, text='Time constant', font=(24)).place(x=90, y=268)
+        self.timeCste = Entry(self.tabLockin, textvariable=self.w4, justify='center', width=10, font=(24))
+        self.timeCste.place(x=220, y=268)
 
-        self.app2 = Button(self.Llock, text="Appliquer", command=self.applock, font=('Courier',15))
-        self.app2.grid(row=4, column=0,padx=10)
+        Label(self.tabLockin, text='Filter', font=(24)).place(x=150, y=335)
+        self.filt = Entry(self.tabLockin, textvariable=self.w5, justify='center', width=10, font=(24))
+        self.filt.place(x=220, y=335)
 
-        self.kill = Button(self.Llock, text="Quitter", command=lambda:self.Llock.destroy(), font=('Courier',15))
-        self.kill.grid(row=4, column=1)
+        Label(self.tabLockin, text='Current', font=(24)).place(x=490, y=67)
+        self.current = Entry(self.tabLockin, textvariable=self.w6, justify='center', width=10, font=(24))
+        self.current.place(x=570, y=67)
 
-    def graph(self):
-        self.Lgraph = LabelFrame(self.para, text="Graphique")
-        self.Lgraph.grid(row=0,column=2)
+        Label(self.tabLockin, text='Offset', font=(24)).place(x=495, y=134)
+        self.offset = Entry(self.tabLockin, textvariable=self.w7, justify='center', width=10, font=(24))
+        self.offset.place(x=570, y=134)
 
-        self.canvas = Canvas(self.Lgraph)
-        self.canvas.grid(row=0,column=0)
+        Label(self.tabLockin, text='Voltage', font=(24)).place(x=485, y=201)
+        self.voltage = Entry(self.tabLockin, textvariable=self.w8, justify='center', width=10, font=(24))
+        self.voltage.place(x=570, y=201)
 
-        self.kill = Button(self.Lgraph, text="Quitter", command=lambda:self.Lgraph.destroy(), font=('Courier',15))
-        self.kill.grid(row=1, column=1)
+        Label(self.tabLockin, text='Current Mode', font=(24)).place(x=445, y=268)
+        self.currentMode = Entry(self.tabLockin, textvariable=self.w9, justify='center', width=10, font=(24))
+        self.currentMode.place(x=570, y=268)
 
-        self.perso = Button(self.Lgraph, text='Personnaliser', command=self.Perso1, font=('Courier',15))
-        self.perso.grid(row=1, column=0)
+        Label(self.tabLockin, text='Source', font=(24)).place(x=490, y=335)
+        self.source = Entry(self.tabLockin, textvariable=self.w10, justify='center', width=10, font=(24))
+        self.source.place(x=570, y=335)
 
-        
-    def courbe(self):
-        return(print("hello"))
+        self.app2 = Button(self.tabLockin, text="Apply", command=self.applock, width=20, height=5, font=(24))
+        self.app2.place(x=320, y=400)
+
+        # Graphic
+
+        self.app3 = Button(self.tabGraphic, text='Start', command=self.graphic(master=self.master),width=10, height=2, font=(24))
+        self.app3.place(x=10, y=10)
 
     def appmot(self):
         self.v1 = float(self.angle.get())
         self.v2 = float(self.pas.get())
         self.v3 = float(self.tps.get())
         self.v4 = float(self.step.get())
-        return(print(self.v1,self.v2,self.v3,self.v4))
+        return (print(self.v1, self.v2, self.v3, self.v4))
 
     def applock(self):
         self.w1 = float(self.amp.get())
         self.w2 = float(self.fq.get())
-        self.w3 = float(self.off.get())
-        self.w4 = float(self.harm.get())
-        return(print(self.w1,self.w2,self.w3,self.w4))
+        self.w3 = float(self.harm.get())
+        self.w4 = float(self.timeCste.get())
+        self.w5 = float(self.filt.get())
+        self.w6 = float(self.current.get())
+        self.w7 = float(self.offset.get())
+        self.w8 = float(self.voltage.get())
+        self.w9 = float(self.currentMode.get())
+        self.w10 = float(self.source.get())
+        return (print(self.w1, self.w2, self.w3, self.w4, self.w5, self.w6, self.w7, self.w8, self.w9, self.w10))
 
-    def mesure(self):
-        return(print('suck it'))
+    def graphic(self, master):
 
-    def Perso1(self):
-        return(print('pffff'))
+        x = np.linspace(0,10,11)
+
+        y = x**2
+
+        self.fig = plt.figure(figsize=(5,5), dpi=100)
+        self.plot = self.fig.add_subplot(111)
+        #self.plot.plot(x,y)
+
+        canvas = FigureCanvasTkAgg(self.fig, master=master)
+        canvas.draw()
+        canvas.get_tk_widget().grid()
+
+        canvas.get_tk_widget().grid()
+
+
 
 root = Tk()
-GUI(root)
+my_gui = Interface(root)
 root.mainloop()
